e4c047438f5e16dd4b2b44c4375c971630bd639a
He Wentao <goodmorning_hwt@sjtu.edu.cn>
2024-02-25 10:22:33 +0800

    add: python packages

--- system/app/geek/lazygit.nix
+++ system/app/geek/lazygit.nix
@@ -10,5 +10,5 @@
 		enable = true;
 	};
 
-  
+
 }
--- system/app/lang/python.nix
+++ system/app/lang/python.nix
@@ -3,7 +3,12 @@
 {
 	home.packages = with pkgs; [
 		python3
+    python310Packages.pip
+    python310Packages.pip-tools
+    python310Packages.jupyter
+    conda
 	];
 
-  
+
+
 }
