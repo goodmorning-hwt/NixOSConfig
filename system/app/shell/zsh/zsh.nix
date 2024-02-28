5aebaa549bf2257c72af825cfbb199f25d3cce9c
He Wentao <goodmorning_hwt@sjtu.edu.cn>
2024-02-28 15:44:45 +0800

    add: conda init shell (for ubuntu only)

--- system/app/shell/zsh/zsh.nix
+++ system/app/shell/zsh/zsh.nix
@@ -75,6 +75,22 @@ if [[ $(hostname) = "hwt-virtual-machine" ]]; then
     source /opt/ros/humble/setup.zsh
 fi
 
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+__conda_setup="$('/home/goodmorninghwt/miniconda3/bin/conda' 'shell.zsh' 'hook' 2> /dev/null)"
+if [ $? -eq 0 ]; then
+    eval "$__conda_setup"
+else
+    if [ -f "/home/goodmorninghwt/miniconda3/etc/profile.d/conda.sh" ]; then
+        . "/home/goodmorninghwt/miniconda3/etc/profile.d/conda.sh"
+    else
+        export PATH="/home/goodmorninghwt/miniconda3/bin:$PATH"
+    fi
+fi
+unset __conda_setup
+# <<< conda initialize <<<
+
+
 		'';
     zplug = {
       enable = true;
