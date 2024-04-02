f3f6285d8091a43e583572112527a87e966df0c2
He Wentao <goodmorning_hwt@sjtu.edu.cn>
2024-04-02 13:35:52 +0800

    add: snp

--- ubuntuAutoDeploy/installV2raya.sh
+++ ubuntuAutoDeploy/installV2raya.sh
@@ -7,7 +7,9 @@ wget -qO - https://apt.v2raya.org/key/public-key.asc | sudo tee /etc/apt/keyring
 
 echo "deb [signed-by=/etc/apt/keyrings/v2raya.asc] https://apt.v2raya.org/ v2raya main" | sudo tee /etc/apt/sources.list.d/v2raya.list
 sudo apt update
-sudo apt install v2raya v2ray ## 也可以使用 xray 包
+sudo snap install v2ray
+sudo snap install v2raya
+# sudo apt install v2raya v2ray ## 也可以使用 xray 包
 
 sudo systemctl start v2raya.service
 sudo systemctl enable v2raya.service
