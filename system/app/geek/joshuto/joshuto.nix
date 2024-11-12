ea83e6e623a921da24e397965896b9ca8648f818
He Wentao <goodmorning_hwt@sjtu.edu.cn>
2024-11-12 18:13:18 +0800

    feat: something

--- system/app/geek/joshuto/joshuto.nix
+++ system/app/geek/joshuto/joshuto.nix
@@ -13,6 +13,12 @@
 
 	
   home.file.".config/joshuto/keymap.toml".source = ./config/keymap.toml; 
+  home.file.".config/joshuto/icons.toml".source = ./config/icons.toml; 
+  home.file.".config/joshuto/joshuto.toml".source = ./config/joshuto.toml; 
+  home.file.".config/joshuto/bookmarks.toml".source = ./config/bookmarks.toml; 
+  home.file.".config/joshuto/mimetype.toml".source = ./config/mimetype.toml; 
+  home.file.".config/joshuto/theme.toml".source = ./config/theme.toml; 
+  home.file.".config/joshuto/preview_file.sh".source = ./config/preview_file.sh; 
 	/*
 	Cheat Sheet:
 	rename: cw
