0cd8a310a77fc3b0663a4e12f41fe60ab46ac882
He Wentao <goodmorning_hwt@sjtu.edu.cn>
2025-03-03 16:13:39 +0800

    feat: fcitx5

--- profiles/hwt-nixos/configuration.nix
+++ profiles/hwt-nixos/configuration.nix
@@ -48,11 +48,13 @@
   };
 
   i18n.inputMethod = {
-    type = "fcitx5";
-    enable = true;
+    enabled = "fcitx5";
     fcitx5.addons = with pkgs; [
       fcitx5-mozc
       fcitx5-gtk
+      fcitx5-nord
+      fcitx5-rime
+      rime-data
     ];
   };
 
