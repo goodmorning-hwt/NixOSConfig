1ae4c3791311d1a9e46958a847e35e5e6348a6bd
hwt-wsl-nixos <hwt-wsl-nixos@nixos.>
2025-03-03 07:36:37 +0000

    feat:remove dae for now

--- profiles/hwt-nixos/configuration.nix
+++ profiles/hwt-nixos/configuration.nix
@@ -2,7 +2,12 @@
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
 
-{ config, pkgs,inputs, ... }:
+{
+  config,
+  pkgs,
+  inputs,
+  ...
+}:
 
 {
   imports = [
@@ -112,18 +117,16 @@
 
   # List packages installed in system profile. To search, run:
   # $ nix search wget
-  environment.systemPackages =
-    with pkgs;
-    [
-      vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-      wget
-      curl
-      git
-    ]
-    ++ (with inputs.daeuniverse.packages.x86_64-linux; [
-      dae
-      daed
-    ]);
+  environment.systemPackages = with pkgs; [
+    vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+    wget
+    curl
+    git
+  ];
+  # ++ (with inputs.daeuniverse.packages.x86_64-linux; [
+  #   dae
+  #   daed
+  # ]);
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
